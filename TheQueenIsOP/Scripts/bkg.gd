9d35aa523786f225345f3d27b7d041572c59eabf
TripleCubes <113813506+TripleCubes@users.noreply.github.com>
2023-09-16 21:34:09 +0700

    Make valid tiles move like wave

--- TheQueenIsOP/Scripts/bkg.gd
+++ TheQueenIsOP/Scripts/bkg.gd
@@ -13,6 +13,9 @@ func _ready():
 		
 	GlobalVars.queen.signal_moved.connect(queue_redraw)
 
+func _process(_delta):
+	queue_redraw()
+
 func _draw():
 	draw_rect(Rect2(-100, -100, WINDOW_WH + 200, WINDOW_WH + 200), Colors.BROWN)
 	
@@ -30,6 +33,21 @@ func _draw():
 
 			draw_rect(Rect2(margin + TILE_WH * x, margin + TILE_WH * y, TILE_WH, TILE_WH), color)
 
+			var t: float = fmod(Time.get_ticks_msec() / float(100), 40)
+			var distance_to_t: float = abs(x + y - t)
+			var h: float = 0
+			h = 2.5 - distance_to_t
+			if h < 0:
+				h = 0
+
+			h /= 2.5
+			h = _ease(h)
+			h *= 3
+
 			if not Engine.is_editor_hint() and GlobalVars.queens_turn \
 			and GlobalVars.queen.get_pos_type(Vector2i(x, y)) != GlobalVars.queen.PosType.INVALID:
-				draw_texture_rect(texture_valid_pos, Rect2(margin + TILE_WH * x, margin + TILE_WH * y, TILE_WH, TILE_WH), false)
+				draw_texture_rect(texture_valid_pos, Rect2(margin + TILE_WH * x, margin + TILE_WH * y - h, TILE_WH, TILE_WH), false)
+
+# From https://easings.net/#easeInOutSine
+func _ease(x) -> float:
+	return -(cos(PI * x) - 1) / 2;
